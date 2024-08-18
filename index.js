02bf4c72b3221bca94e97d953a51bf458637ca5d
ManojSai <2100032192@kluniversity.com>
2024-08-18 14:43:47 +0530

    first time pusing my project to git

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotEnv = require('dotenv');
+const mongoose = require('mongoose');
+const vendorRoutes = require('./routes/vendorRoutes.js');
+const bodyParser = require('body-parser');
+const firmRoutes = require('./routes/firmRoutes');
+const productRoutes = require('./routes/productRoutes');
+const path = require('path')
+const cors = require('cors');
+
+const app = express()
+
+
+const PORT = 4000;
+
+dotEnv.config();
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("mongodb connected successfully"))
+.catch((error) => console.log(error))
+
+app.use(bodyParser.json());
+app.use('/vendor',vendorRoutes);
+app.use('/firm', firmRoutes)
+app.use('/product',productRoutes)
+app.use('/uploads', express.static('uploads'));
+
+
+
+app.listen(PORT,() =>{
+    console.log(`server started running successfully ${PORT}`)
+})
+
+app.use('/home',(req,res) => {
+    res.send("<h1>Welcome to SUBY</h1>")
+})
